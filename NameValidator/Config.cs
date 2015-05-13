d3a2c22f9258e136a9972f1839acad560aedf187
Enerdy <rodrigo.rente@live.com>
2015-05-13 18:42:20 +0100

    Initial release.

---
+++ NameValidator/Config.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI;
+using Newtonsoft.Json;
+
+namespace NameValidator
+{
+	public class Config
+	{
+		/// <summary>
+		/// The action to take against players carrying invalid nicknames.
+		/// Valid actions: 'kick', 'ban', 'any message to send to the player'.
+		/// </summary>
+		public string Action = "kick";
+
+		/// <summary>
+		/// The reason to use when the action is set to kick/ban.
+		/// </summary>
+		public string Reason = "Invalid character name.";
+
+		/// <summary>
+		/// A string containing chars a character name can't have.
+		/// </summary>
+		public string InvalidChars = "";
+
+		/// <summary>
+		/// A regex to match character names against. A matching name is deemed as invalid.
+		/// </summary>
+		public string InvalidNameRegex = "";
+
+		/// <summary>
+		/// Whether to kick any player using characters that the Terraria font doesn't natively support.
+		/// </summary>
+		public bool TerrariaFontOnly = false;
+
+		public static Config Read(string path)
+		{
+			try
+			{
+				Directory.CreateDirectory(Path.GetDirectoryName(path));
+				if (!File.Exists(path))
+				{
+					Config config = new Config();
+					File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented));
+					return config;
+				}
+				return JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
+			}
+			catch (Exception ex)
+			{
+				TShock.Log.ConsoleInfo(ex.Message);
+				TShock.Log.Error(ex.ToString());
+				return new Config();
+			}
+		}
+	}
+}
---
+++ NameValidator/NameValidator.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Windows.Media;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace NameValidator
+{
+	[ApiVersion(1, 17)]
+	public class NameValidator : TerrariaPlugin
+	{
+		// If this fails, rip
+		private FontFamily font;
+
+		public override string Author
+		{
+			get { return "Enerdy"; }
+		}
+
+		public override string Description
+		{
+			get { return "Validate character names on join based on a configuration file."; }
+		}
+
+		public override string Name
+		{
+			get { return "Name Validator"; }
+		}
+
+		public override Version Version
+		{
+			get { return Assembly.GetExecutingAssembly().GetName().Version; }
+		}
+
+		private Config Config { get; set; }
+
+		public NameValidator(Main game)
+			: base(game)
+		{
+			Order = 10;
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+			}
+		}
+
+		public override void Initialize()
+		{
+			string fontname = "Andy";
+			if ((font = Fonts.SystemFontFamilies.FirstOrDefault(f => f.Source.Equals(fontname, StringComparison.OrdinalIgnoreCase))) == null)
+				TShock.Log.ConsoleError("The font '{0}' was not found.", fontname);
+
+			string path = Path.Combine(TShock.SavePath, "NameValidator.json");
+			Config = Config.Read(path);
+
+			ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+
+			Commands.ChatCommands.Add(new Command("namevalidator.reload", (args) =>
+			{
+				Config = Config.Read(path);
+				args.Player.SendSuccessMessage("Name Validator config reloaded.");
+			}, "nvreload"));
+		}
+
+		private void OnJoin(JoinEventArgs e)
+		{
+			if (e.Handled || e.Who < 0 || e.Who > Main.player.Length - 1)
+				return;
+
+			Player player = Main.player[e.Who];
+			if (player != null)
+			{
+				string name = player.name;
+				if (!ValidateString(name))
+				{
+					switch (Config.Action.ToLowerInvariant())
+					{
+						case "ban":
+							TShock.Utils.Ban(TShock.Players[e.Who], Config.Reason);
+							TShock.Log.ConsoleInfo("Player '{0}' was banned for \"{1}\".", name, Config.Reason);
+							e.Handled = true;
+							return;
+						case "kick":
+							TShock.Utils.Kick(TShock.Players[e.Who], Config.Reason, silent: true);
+							TShock.Log.ConsoleInfo("Player '{0}' was kicked for \"{1}\".", name, Config.Reason);
+							e.Handled = true;
+							return;
+						default:
+							TShock.Players[e.Who].SendWarningMessage(Config.Action);
+							return;
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Validates a string according to a configuration file.
+		/// </summary>
+		/// <param name="s">The string to validate</param>
+		/// <returns>Whether the string is valid.</returns>
+		private bool ValidateString(string s)
+		{
+			// Font contains check
+			if (Config.TerrariaFontOnly)
+			{
+				foreach (char c in s)
+				{
+					ICollection<Typeface> typefaces = font.GetTypefaces();
+					GlyphTypeface glyph;
+					foreach (Typeface t in typefaces)
+					{
+						t.TryGetGlyphTypeface(out glyph);
+						if (glyph != null && !glyph.CharacterToGlyphMap.ContainsKey(Convert.ToInt16(c)))
+						{
+							// Spot detected
+							return false;
+						}
+					}
+				}
+			}
+
+			// Regex check
+			if (!String.IsNullOrWhiteSpace(Config.InvalidNameRegex) && Regex.IsMatch(s, Config.InvalidNameRegex))
+				return false;
+
+			// Invalid char check and return if valid
+			return !Config.InvalidChars.Intersect(s).Any();
+		}
+	}
+}
